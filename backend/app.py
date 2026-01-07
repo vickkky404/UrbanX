8a1fcc129e803a5003ff91c46e6cc5369cf537f3
nalin <nalinikantbastia@gmail.com>
2026-01-07 12:06:26 +0530

    Learning the structure of the backend flask app

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,15 @@
+# importing flask modole and calling a perticular flask file
 from flask import Flask
 
-
+# creates the instance for the flask  class
+#  app = Flask(__name__) determines the class path
 app = Flask(__name__)
 
+# The route() decorator tells Flask what URL should trigger the function
 @app.route('/')
 def home():
     return "UrbanX Backend Implementation"
-
+#  ///////////////////
+# runs the applicaiton
 if __name__ == "__main__":
     app.run(debug=True)
